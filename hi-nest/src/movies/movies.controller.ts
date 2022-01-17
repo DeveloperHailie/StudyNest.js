a61f10452bd3fad636e490dcf48bfd0e1bcdd9e3
hailie <20181011@sungshin.ac.kr>
2022-01-18 02:47:59 +0900

    [add] DTOs와 유효성 검사 추가

--- hi-nest/src/movies/movies.controller.ts
+++ hi-nest/src/movies/movies.controller.ts
@@ -1,34 +1,45 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
 
-    constructor(private readonly moviesService: MoviesService){}
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
 
-    @Get()
-    getAll(): Movie[]{
-        return this.moviesService.getAll();
-    }
+  @Get(':id')
+  getOne(@Param('id') movieId: number): Movie {
+    return this.moviesService.getOne(movieId);
+  }
 
-    @Get(":id")
-    getOne(@Param("id") movieId: string): Movie{
-        return this.moviesService.getOne(movieId);
-    }
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
+  }
 
-    @Post()
-    create(@Body() movieData){
-        return this.moviesService.create(movieData);
-    }
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
+    return this.moviesService.deleteOne(movieId);
+  }
 
-    @Delete(":id")
-    remove(@Param("id") movieId: string){
-        return this.moviesService.deleteOne(movieId);
-    }
-
-    @Patch(':id')
-    path(@Param('id') movieId: string, @Body() updateData){
-        return this.moviesService.update(movieId, updateData);
-    }
+  @Patch(':id')
+  path(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateData);
+  }
 }
