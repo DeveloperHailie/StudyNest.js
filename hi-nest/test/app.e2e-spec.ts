5edc9b526e932c888a25a1b093a2203338a31782
hailie <20181011@sungshin.ac.kr>
2022-01-22 23:30:37 +0900

    [update] beforeAll로 변경 및 todo 작성

--- hi-nest/test/app.e2e-spec.ts
+++ hi-nest/test/app.e2e-spec.ts
@@ -6,7 +6,7 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -44,5 +44,11 @@ describe('AppController (e2e)', () => {
         .delete("/movies")
         .expect(404); 
     });
+  });
+
+  describe('/movies/:id',() => {
+    it.todo("GET")
+    it.todo("DELETE");
+    it.todo("PATCH");
   });
 });
